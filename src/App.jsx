90aa3bb49454a20a34121d01642c9de11d754304
Cauê Santos <cauedossantossilva@hotmail.com>
2022-06-05 08:16:20 -0300

    feat: create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import Post from './Post'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
   return (
     <div>
       <Header />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
