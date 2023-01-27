e2256f44140d6b674cca36c7aa57efd20a643f75
Cauê Santos <cauedossantossilva@hotmail.com>
2023-01-27 17:19:49 -0300

    feat: added img extends interface

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { ImgHTMLAttributes } from 'react';
 
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
-  src: string;
-  alt?: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
 }
 
-function Avatar({ src, alt, hasBorder = true }: AvatarProps) {
+function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img 
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
